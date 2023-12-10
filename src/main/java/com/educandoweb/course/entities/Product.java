0207fe26e16ff43fec74bc85b43dfd4ac9250950
laurohms <laurohenriquemsantos@gmail.com>
2023-12-10 19:52:37 -0300

    many-to-many jointable association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -24,9 +27,12 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToAny
+    @JoinTable(name = "product_category", joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    
     public Product() {
     }
 
@@ -37,7 +43,7 @@ public class Product implements Serializable {
         this.price = price;
         this.imgUrl = imgUrl;
     }
-
+    
     public Long getId() {
         return id;
     }
@@ -49,7 +55,7 @@ public class Product implements Serializable {
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -73,14 +79,15 @@ public class Product implements Serializable {
     public String getImgUrl() {
         return imgUrl;
     }
-
+    
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
     }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
+    
+    public Set<Category> getCategories() {
+        return categories;
     }
+    
 
     @Override
     public int hashCode() {
